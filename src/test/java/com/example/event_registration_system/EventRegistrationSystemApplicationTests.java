7ec1f74626fbe814df9bfda8fedf7f123e47199c
Michael The <72528@fif.co.id>
2025-09-08 10:45:05 +0700

    First Commit

---
+++ src/test/java/com/example/event_registration_system/EventRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
